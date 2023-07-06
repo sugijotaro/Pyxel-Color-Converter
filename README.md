4501b8fc16f60f2a9bf3128c0afeb42a4f492c15
Jotaro Sugiyama <sugi.jotaro@gmail.com>
2023-07-06 14:06:02 +0900

    add --resize option

--- README.md
+++ README.md
@@ -11,12 +11,21 @@ Python 3と、`PIL`、`numpy` ライブラリが必要です。
 ```bash
 python pyxel_color_converter.py input_image.png
 ```
-ここで、input_image.pngは変換したい画像のファイル名（パス）です。同じディレクトリに converted_image.png という名前で新しい画像が作成されます。新しい画像は、Pyxelの16色のパレットに変換されます。
+
+ここで、input_image.pngは変換したい画像のファイル名（パス）です。同じディレクトリに converted_image.pngという名前で新しい画像が作成されます。新しい画像は、Pyxel の 16 色のパレットに変換されます。
+
+さらに、画像の縦または横が256ピクセルを超える場合、画像をリサイズすることもできます。この場合、以下のように--resizeオプションを使用してください。
+
+```bash
+python pyxel_color_converter.py --resize input_image.png
+```
 
 ---
+
 Pyxel Color Converter is a tool to convert any image to the 16-color palette available in [Pyxel](https://github.com/kitao/pyxel).
 
 ## Usage
+
 You need Python 3 and the PIL and numpy libraries.
 
 To convert an image, run the following command from the terminal (command line):
@@ -24,4 +33,11 @@ To convert an image, run the following command from the terminal (command line):
 ```bash
 python pyxel_color_converter.py input_image.png
 ```
+
 Here, input_image.png is the filename (or path) of the image you want to convert. A new image will be created in the same directory with the name converted_image.png. This new image is converted to the 16-color palette of Pyxel.
+
+Moreover, if the width or height of your image exceeds 256 pixels, you have the option to resize the image. To do so, use the --resize option like this:
+
+```bash
+python pyxel_color_converter.py --resize input_image.png
+```
--- pyxel_color_converter.py
+++ pyxel_color_converter.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import numpy as np
 import argparse
-
+import sys
 
 def closest_color(rgb, color_list):
     r, g, b = rgb
@@ -12,9 +12,8 @@ def closest_color(rgb, color_list):
         color_diffs.append(color_diff)
     return color_list[color_diffs.index(min(color_diffs))]
 
-
-def convert_image(image_path):
-    colors = [(0, 0, 0), (43, 51, 95), (126, 32, 114), (25, 149, 156), (139, 72, 82),
+def convert_image(image_path, resize):
+    colors = [(0, 0, 0), (43, 51, 95), (126, 32, 114), (25, 149, 156), (139, 72, 82), 
               (57, 92, 152), (169, 193, 255), (238, 238, 238), (212, 24, 108),
               (211, 132, 65), (233, 195, 91), (112, 198, 169), (118, 150, 222),
               (163, 163, 163), (255, 151, 152), (237, 199, 176)]
@@ -22,22 +21,35 @@ def convert_image(image_path):
     img = Image.open(image_path)
     img = img.convert("RGB")
 
+    if resize:
+        width, height = img.size
+        if max(width, height) > 256:
+            if width > height:
+                new_width = 256
+                new_height = int(256 * (height / width))
+            else:
+                new_height = 256
+                new_width = int(256 * (width / height))
+            img = img.resize((new_width, new_height))
+
     data = np.array(img)
     new_data = np.zeros_like(data)
 
     for i in range(data.shape[0]):
         for j in range(data.shape[1]):
             new_data[i][j] = closest_color(data[i][j], colors)
+            if j % 100 == 0:
+                print("\rProcessing... {:.2f}%".format(100 * (i * data.shape[1] + j) / (data.shape[0] * data.shape[1])), end="")
+
+    print("\rProcessing... done!    ")
 
     new_img = Image.fromarray(new_data, "RGB")
     new_img.save("converted_image.png")
 
-
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Converts an image to closest colors in Pyxel palette.")
-    parser.add_argument("image_path", type=str,
-                        help="Path to the image to be converted.")
+    parser = argparse.ArgumentParser(description="Converts an image to closest colors in Pyxel palette.")
+    parser.add_argument("image_path", type=str, help="Path to the image to be converted.")
+    parser.add_argument("--resize", action='store_true', help="Resizes the image to max 256 pixels if either width or height is larger.")
 
     args = parser.parse_args()
-    convert_image(args.image_path)
+    convert_image(args.image_path, args.resize)
